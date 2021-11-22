24336ec623c039b8b48c16f31396a5836a7f26f2
Tony <TonyAleynikov@gmail.com>
2021-11-22 13:07:45 +0300

    Предварительно завершены тесты. Исправили старые тесты.

--- spec/rl_str_gen_spec.rb
+++ spec/rl_str_gen_spec.rb
@@ -1,7 +1,8 @@
 require "rspec"
 require_relative "../app/methods"
 
-describe rl_str_gen do
+describe "rl_str_gen" do
+
 
   it "should return a string" do
     1000.times do
@@ -209,16 +210,55 @@ describe rl_str_gen do
                 .each do |el|
                 unless el.match?(/\A[А-ЯЁ]{2,}\z/)
                 found = el.scan(/[аоуэыияеёю]/i).size
-                calc  = (el.size * 0.4).to_i
+                calc  = ((el.size - el.scan(/[ъь]/i).size) * 0.4).to_i
                 res   = found >= calc ? ">=#{calc} vowels" : "#{found} vowels"
                 expect([res, el]).to eq([">=#{calc} vowels", el])
-
         end
       end
     end
   end
 
 
-  it "should ??? vowel in single-syllable words" do
+  it "should contain 5 or less consonants in single-syllable words" do
+    1000.times do
+      rl_str_gen.gsub(/[^а-яё -]/i, "")
+                .split
+                .reject { |w| w.match?(/-|[аоуэыияеёю].*[аоуэыияеёю]|/i) ||
+                              w.match?(/\A[А-ЯЁ]{2,}\z/)  }
+                .each do |word|
+                  expect(word.size).to be <= 6
+      end
+    end
   end
+
+
+  it "should allow only я, е, ё, ю after ъ" do
+    1000.times do
+      expect(rl_str_gen.gsub(/\b[А-ЯЁ]{2,}\b/, "").match(/ъ[^яюеё]/i)).to be_nil
+    end
+  end
+
+
+  it "should not allow a vowel add the beginning of the word"\
+      " in single-syllable words if they have 3 or more letters" do
+    1000.times do
+      rl_str_gen.gsub(/[^а-яё -]/i, "")
+                .split
+                .reject { |w| w.match?(/-|([аоуэыияеёю].*[аоуэыияеёю])/i) ||
+                              w.match?(/\A[А-ЯЁ]{2,}\z/) || w.size < 3 }
+                .each do |word|
+                  expect(word).to match(/\A[^аоуэыияеёю]/i)
+      end
+    end
+  end
+
+
+  it "should for bit Ъ and Ь in acronyms" do
+    1000.times do
+      expect(rl_str_gen.match(/(?=\b[А-ЯЁ]{2,}\b)\b[А-ЯЁ]*[ЪЬ][А-ЯЁ]*\b/))
+                       .to be_nil
+    end
+  end
+
+
 end
